7800b618441dc08a6b283e64c66cf954c691cec5
Romano Livi <romano_livi@outlook.com>
2021-01-13 22:34:59 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,16 +20,17 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = title = "Home Alone", release_date = 1990)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
   
   Movie.create do |m|
-    __
+    m.title = "Home Alone"
+    m.release_date = 1991
   end
 end
 
