b3933ee431739f1382d188626d24c6d7432eebe4
Romano Livi <romano_livi@outlook.com>
2021-01-13 22:46:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -92,7 +92,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   new_movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  new_movie
+  new_movie.destroy
 end
 
 def can_destroy_all_items_at_once
