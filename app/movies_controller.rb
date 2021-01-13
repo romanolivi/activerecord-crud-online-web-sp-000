db11668fd454698ee9c4f85c6bb7b91b3c6cb8a3
Romano Livi <romano_livi@outlook.com>
2021-01-13 22:47:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = title = "Home Alone", release_date = 1990)
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1991
+    m.title = "The Room"
+    m.release_date = 1990
   end
 end
 
