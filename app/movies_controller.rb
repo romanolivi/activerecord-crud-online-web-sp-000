57954e93daf340d35bd8e4fcb06af0c15f5e7f30
Romano Livi <romano_livi@outlook.com>
2021-01-13 22:45:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -86,13 +86,13 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+ Movie.all.update(title: "A Movie")
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  new_movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  new_movie
 end
 
 def can_destroy_all_items_at_once
