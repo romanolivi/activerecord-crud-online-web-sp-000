e2439aa2d685f66741028796f6d184ac35bc4077
Romano Livi <romano_livi@outlook.com>
2021-01-13 22:41:48 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -77,8 +77,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  new_movie = Movie.find_by(title: "Wat?")
+  new_movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
